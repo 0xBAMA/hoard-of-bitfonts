123c746ca33a54b86d9e9d393d26abe898de6350
0xBAMA <jon.baker1222@gmail.com>
2021-08-15 05:29:39 -0400

    letters layout

---
+++ letters.h
@@ -0,0 +1,26 @@
+#include <vector>
+#include <random>
+#include "json.h"
+
+using json = nlohmann::json;
+
+class letter {
+public:
+	std::vector<std::vector<int>> data;
+}
+
+class letter_selector {
+public:
+	void populate(std::vector<unsigned char> &data, int count, float color[4]){
+		// load the model into the list of letters
+		json j;
+		std::vector<letter> letters;
+
+		// begin to pick letters
+
+
+
+
+
+	}
+}
