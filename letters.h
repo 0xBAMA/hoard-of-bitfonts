0c74f42edaa800f80edc1ac871933007031e049e
0xBAMA <jon.baker1222@gmail.com>
2021-08-17 13:36:18 -0400

    partial implementation of the scaling stuff, I'll be returning to this for the spaceship thing

--- letters.h
+++ letters.h
@@ -2,9 +2,10 @@
 #include <random>
 #include <fstream>
 #include <iostream>
-#include "json.h"
 
+#include "json.h"
 using json = nlohmann::json;
+
 
 class letter {
 public:
@@ -72,24 +73,56 @@ public:
 		data[index+3]=color[3];
 	}
 
-	void stamp(letter l, int x, int y, int z, unsigned char color[4], std::vector<unsigned char> &data, int dim) {
+	void stamp(letter l, int x, int y, int z, int dir, int scale, unsigned char color[4], std::vector<unsigned char> &data, int dim) {
 		// use the write function to write at all voxels where the currently selected letter has 1's
 
 		int xdim = l.data.size();
 		int ydim = l.data[1].size();
 
 		std::cout << "stamping at " << x << " " << y << " with dimensions " << xdim << " " << ydim << std::endl;
-
 		for(int xx = 0; xx < xdim; xx++)
 		for(int yy = 0; yy < ydim; yy++)
-			if(l.data[xx][yy]==1)
-				write(x+xx,y+yy,z,color,data,dim);
+		for(int xs = 0; xs < scale; xs++)
+		for(int ys = 0; ys < scale; ys++)
+		for(int zs = 0; zs < scale; zs++)
+			switch(dir) {
+				case 1:
+					if(l.data[xx][yy]==1)
+						write(x+xx*scale+xs, y+yy*scale+ys, z+zs,color,data,dim);
+					break;
 
-		// randomize orientation
+				case 2:
+					if(l.data[xx][yy]==1)
+						write(x-xx+xs, y+yy*scale+ys, z+zs,color,data,dim);
+					break;
+
+				case 3:
+					if(l.data[xx][yy]==1)
+						write(x+xx*scale+xs, y+yy*scale+ys, z+zs,color,data,dim);
+					break;
+
+				case 4:
+					if(l.data[xx][yy]==1)
+						write(x+xx*scale+xs, y-yy+ys, z+zs,color,data,dim);
+					break;
+
+				case 5:
+					if(l.data[xx][yy]==1)
+						write(x+xs, y+yy*scale+ys, z+xx*scale+zs,color,data,dim);
+					break;
+
+				case 6:
+					if(l.data[xx][yy]==1)
+						write(x+xs, y+yy*scale+ys, z-xx+zs,color,data,dim);
+					break;
+
+				default:
+					break;
+			}
 	}
 
 	std::vector<letter> letters;
-	void populate(std::vector<unsigned char> &data, int dim, int count, float color[4]){
+	void populate(std::vector<unsigned char> &data, int dim, int count, int num_dirs, float color[4]){
 		// load the model into the list of letters
 		std::ifstream i("resources/hoard-of-bitfonts/optimized.json");
 		// std::ifstream i("monolithic_model.json");
@@ -131,9 +164,11 @@ public:
 		std::seed_seq s{r(), r(), r(), r(), r(), r(), r(), r(), r()};
 		gen = std::mt19937_64(s);
 
-		std::uniform_int_distribution<int> colo(-5, 5);
+		std::uniform_int_distribution<int> colo(-15, 15);
 		std::uniform_int_distribution<int> pick(0, letters.size());
 		std::uniform_int_distribution<int> loc(0, dim);
+		std::uniform_int_distribution<int> dir(1, (num_dirs%6)+1);
+		std::uniform_int_distribution<int> sca(1, (num_dirs/6)+1);
 
 		unsigned char col[4];
 
@@ -144,7 +179,7 @@ public:
 			for(int j = 0; j < 4; j++)
 				col[j] = (color[j]*255)+colo(gen);
 
-			stamp(letters[pick(gen)], loc(gen), loc(gen), loc(gen), col, data, dim);
+			stamp(letters[pick(gen)], loc(gen), loc(gen), loc(gen), dir(gen), sca(gen), col, data, dim);
 		}
 	}
 };
